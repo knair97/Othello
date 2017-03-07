d57851a2193fbfc264c41afc88bc6c8efbabedc1
Karthik Nair <karthiknair97@live.com>
2017-03-06 20:05:41 -0800

    small change

--- player.cpp
+++ player.cpp
@@ -54,7 +54,7 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */
-    
-    board.doMove( opponentsMove, opponent_side);
+    board.doMove(opponentsMove, opponent_side);
+    board.doMove(opponentsMove, opponent_side);
     return nullptr;
 }
